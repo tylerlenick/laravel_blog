489da3652103921475860b91bb7345fce56f35d3
tylenick <tylerlenick@gmail.com>
2018-11-04 19:42:27 -0500

    more auth check

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -84,6 +84,11 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if(auth()->user->id !== $post->user_id){
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        } 
         return view('posts.edit')->with('post', $post);
     }
 
@@ -119,6 +124,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if(auth()->user->id !== $post->user_id){
+            return redirect('/posts')->with('error', 'Unauthorized Page');
+        } 
+
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,11 +11,13 @@
     <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
     <hr>
     @if(!Auth::guest())
+        @if(Auth::user()->id == $post->user_id)
         <a href="/laravel_blog/public/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
         
         {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
             {{Form::hidden('_method', 'delete')}}
             {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
         {!!Form::close() !!}
+        @endif
     @endif
 @endsection
